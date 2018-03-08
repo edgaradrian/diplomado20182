29390ccb77aad04d5917cba516b9571e6bf32b9b
Edgar Adrián <36165300+edgaradrian@users.noreply.github.com>
2018-03-07 22:54:04 -0600

    Tarea capitulo 3
    Tarea del libro, capitulo 3

--- diplomadoTareas/WorldTrotter/WorldTrotter/ViewController.swift
+++ diplomadoTareas/WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    /*override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, heigth: 150)
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
         let firstView = UIView(frame: firstFrame)
-        
         firstView.backgroundColor = UIColor.blue
         view.addSubview(firstView)
         
-        
-    }//viewDidLoad
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+        firstView.addSubview(secondView)
+    }*///viewDidLoad
 
 }//ViewController
 
